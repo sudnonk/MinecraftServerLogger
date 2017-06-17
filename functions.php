c01c2272057dfcefc85be56110a32c91b034db44
Yuki <shipyuuki@yahoo.co.jp>
2017-06-17 14:51:05 +0900

    initial commit. v0.1

---
+++ functions.php
@@ -0,0 +1,71 @@
+<?php
+//コンフィグファイルの場所
+define('INI_PATH','./config.ini');
+
+$util = new Util();
+
+/**
+ * Class Util
+ */
+class Util{
+    private $db_ini;
+    private $mc_ini;
+
+    /**
+     * Util constructor.
+     * エラーが起きたらexit
+     */
+    function __construct(){
+        $ini = parse_ini_file(INI_PATH,true);
+        if($ini === false) exit("Failed to open config file. Check your config file exists.");
+        $this->db_ini = $ini["MySQL"];
+        $this->mc_ini = $ini["Minecrafat"];
+    }
+
+    /**
+     * @param $key string iniのkey
+     * @param $required bool そのキーが必須か
+     * @return $value string iniのvalue
+     */
+    function get_db_ini_value($key,$required = true){
+        $value = $this->db_ini[$key];
+        if($value !== false || $required !== true) return $value;
+        exit("$key not found. Check your config.");
+    }
+    function get_mc_ini_value($key,$required = true){
+        $value = $this->mc_ini[$key];
+        if($value !== false || $required !== true) return $value;
+        exit("$key not found. Check your config.");
+    }
+
+    /**
+     * @return string
+     * エラーが起きたらexit
+     */
+    function get_log_file(){
+        $log = file_get_contents($this->get_mc_ini_value("LOG_PATH"));
+        if($log !== false) return $log;
+        exit("Failed to open log file. Check your log file exists as you configured.");
+    }
+
+    /**
+     * @param $log string ログファイル
+     * @return array
+     * エラーが起きたらexit
+     */
+    function parse_log_file($log){
+        $lines = preg_split("\n",$log);
+        if($lines !== false) return $lines;
+        exit("Failed to parse log file. Check error_log.");
+    }
+
+    function connect(){
+        try {
+            $dsn = "mysql:host=" . $this->get_db_ini_value("DB_HOST") . ";dbname=" . $this->get_db_ini_value("DB_NAME");
+            $pdo = new PDO($dsn, $this->get_db_ini_value("DB_USER"), $this->get_db_ini_value("DB_PASS"));
+            return $pdo;
+        }catch (PDOException $e){
+            exit("Failed to connect database. The PDO message is ".$e->getMessage());
+        }
+    }
+}
---
+++ import.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * ログファイルをパースして、新しく追加された行のみデータベースに入れます。
+ */
+require_once "functions.php";
+
+/* @var $log_file string ログファイル */
+$log_file = $util->get_log_file();
+/* @var $lines array ログファイルの各行 */
+$lines = $util->parse_log_file($log_file);
+
+/* @var $pdo PDO  */
+$pdo = $util->connect();
+
+/* @var $date string 今日の日付 */
+$date = date("Y-m-d");
+/* @var $exits array すでに存在しているid */
+try {
+    $exits = $pdo->query("select id from logs where id like '$date%'")->fetchAll(PDO::FETCH_COLUMN);
+}catch (PDOException $e){
+    exit("Failed to get ids. PDO message is ".$e->getMessage());
+}
+$exits = array_flip($exits);
+
+/**
+ * @var  $key int 行番号-1
+ * @var  $line string
+ */
+foreach ($lines as $key=>$line) {
+    /* @var $match array */
+    preg_match("/^(\[[0-9]{2}:[0-9]{2}:[0-9]{2}\]) (\[.+\]): (.+)$/", $line, $match);
+
+    /**
+     * 例：[03:38:00] [Server thread/WARN]: Fetching addPacket for removed entity
+     * @var $time string "03:38:00"
+     * @var $status string "Server thread/WARN"
+     * @var $message string "Fetching addPacket for removed entity"
+     */
+    $time = $match[1];
+    $status = $match[2];
+    $message = $match[3];
+
+    /* @var $id string 日付+ログされた時間+行番号は恐らく一意 */
+    $id = $date.$time.$key;
+
+    if(isset($exits[$id])){
+        //すでにそのidが存在していればスキップ
+        continue;
+    }else{
+        //存在していなければ$existsに追加
+        $exits[$id] = true;
+    }
+
+    try {
+        $stmt = $pdo->prepare("insert into logs(`id`,`time`,`status`,`message`) VALUES (:id,:time,:status,:message)");
+        $stmt->bindValue(":id", $id, PDO::PARAM_STR);
+        $stmt->bindValue(":time", $time, PDO::PARAM_STR);
+        $stmt->bindValue(":status", $status, PDO::PARAM_STR);
+        $stmt->bindValue(":message", $message, PDO::PARAM_STR);
+        $stmt->execute();
+    }catch (PDOException $e){
+        exit("Failed to insert log. PDO message is ".$e->getMessage());
+    }
+}
